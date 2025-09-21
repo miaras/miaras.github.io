8c9d2b20ae2b4f36e3baddbacbf6f01583ba56bf
Jongmin Baek <server@stelland.io>
2025-09-21 21:46:03 +0200

    Use html, not php, for github pages

--- header.php
+++ header.php
@@ -1,15 +1,2 @@
 <!-- header.php -->
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="stylesheet.css">
-    <script src="script.js" defer></script>
-  <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=TAG_ID"></script>
-  <header>
-    <a href="index.php">Home</a>
-    <a href="writing.php">Writing</a>
-    <a href="https://miarasung.substack.com">Substack</a>
-  </header>
-</head>
 
