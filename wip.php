c74a342acfc7ec930142a7218e710a4c7f44f2c5
Miara Sung <jbaek080@berkeley.edu>
2025-11-14 02:12:45 +0100

    get rid of phps, get blog entries to work cleanly

--- wip.php
+++
@@ -1,5 +0,0 @@
-<div id="In praise of microtransgressions" class="05282022">
-  05282022
-  In praise of microtransgressions
-  If we can't abolish the cops with a stroke of a pen, we should DDOS their attention with microtransgressions so they have less time to kill people (granted you have the privilege you'll be safe as a consequence).
-</div
